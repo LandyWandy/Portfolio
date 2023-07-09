9341535b769cc3ba6f589f81c1c015317ceed928
Landon Heaney <landonheaney@gmail.com>
2023-07-08 23:49:39 -0500

    nav props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React, { useState, useCallback} from 'react';
 import Rain from './components/rain/rain.js'
 import Header from './components/header/header.js'
 import Nav from './components/nav/nav.js'
 
 function App() {
+  const [menuState, setMenuState] = useState(false);
+
+  const toggleMenu = useCallback(() => {
+    setMenuState(state => state ? (state === 'deactive' ? 'active' : 'deactive') : 'active');
+  }, []);
   return (
-    <div>
-      <Nav/>
-      <Rain/>
-      <Header/>
-    </div>
+    <React.Fragment>
+      <Nav toggleMenu={toggleMenu} showMenu={menuState} />
+      <Rain />
+      <Header />
+    </React.Fragment>
   );
 }
 
