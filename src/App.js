265d1529b26a95172a66b0a74d9a34fa265ad0be
Landon Heaney <landonheaney@gmail.com>
2023-08-01 16:41:28 -0500

    about section and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useCallback} from 'react';
-import NavMenu from './components/navMenu/navMenu.js';
-import Rain from './components/rain/rain.js'
-import Header from './components/header/header.js'
-import Nav from './components/nav/nav.js'
+import React, { useState, useEffect, useCallback} from 'react';
+import NavMenu from './components/navMenu/navMenu.jsx';
+import Rain from './components/rain/rain.jsx'
+import Header from './components/header/header.jsx'
+import Nav from './components/nav/nav.jsx'
+import About from './components/about/about.jsx'
 
 
 function App() {
@@ -14,12 +15,44 @@ function App() {
 
   console.log(menuState)
 
+  useEffect(() => {
+    const navbar = document.querySelector('#navbar');
+    const header = document.querySelector('#welcome');
+
+    window.onscroll = () => {
+      let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (scrollPos - 100 <= window.innerHeight)
+        header.style.visibility = header.style.visibility === 'hidden' && 'visible';
+      else header.style.visibility = 'hidden';
+
+      if (scrollPos + 100 >= window.innerHeight) navbar.classList.add('bg-active');
+      else navbar.classList.remove('bg-active');
+    };
+
+    (function navSmoothScrolling() {
+      const internalLinks = document.querySelectorAll('a[href^="#"]');
+      for (let i in internalLinks) {
+        if (internalLinks.hasOwnProperty(i)) {
+          internalLinks[i].addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(internalLinks[i].hash).scrollIntoView({
+              block: 'start',
+              behavior: 'smooth'
+            });
+          });
+        }
+      }
+    })();
+  }, []);
+
   return (
     <React.Fragment>
       <NavMenu toggleMenu={toggleMenu} showMenu={menuState}/>
       <Nav toggleMenu={toggleMenu} showMenu={menuState} />
       <Rain />
       <Header />
+      <About />
     </React.Fragment>
   );
 }
---
+++ src/components/about/about.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './about.css';
+
+const About = props => {
+  return (
+    <section id="about">
+      <div className="wrapper">
+        <article>
+          <div className="title">
+            <h3>Who is this guy?</h3>
+            <p className="separator" />
+          </div>
+          <div className="desc full">
+            <h4 className="subtitle">My name is Landon Heaney.</h4>
+            <p>
+              I am a Full-Stack Developer based in Austin, Texas.
+            </p>
+            <p>
+            I have two years of experience in technical operations management and recently leveled up with a Full-Stack Development certification. Don't be fooled by the tech talk though - I've also done my time behind an espresso machine. Whether it's perfecting a double shot or a chunk of JavaScript, I love a good challenge. When I'm not diving into code or reminiscing about my barista days, you'll likely find me scaling a rock wall at the local bouldering gym. And on those quiet rest days, you can catch me watching a gripping horror movie with my cat.
+            </p>
+          </div>
+          <div className="title">
+            <h3>What does he do?</h3>
+            <p className="separator" />
+          </div>
+          <div className="desc">
+            <h4 className="subtitle">I'm a programmer.</h4>
+            <p>
+              For the front-end I usually work with Javascript, React, HTML5 and CSS3. I have experience with Electron, JQuery, Bootstrap, and version control with Git. 
+            </p>
+            <p>
+              For the back-end I work with Node.js, and am experienced with MongoDB, MySQL, GraphQL and Jest. I also have a firm foundation in database management, network management, and the use of Bash and Powershell scripts for bug troubleshooting and fixes.
+            </p>
+          </div>
+          <div className="desc">
+            <h4 className="subtitle">Also a project manager.</h4>
+            <p>
+              I have managed the uptime for a network of 21,000 devices nationwide at GroceryTV, effectively discovering and resolving device-related issues. I also developed software for and led a project to deliver firmware updates to a shipment of 2,000 devices within a week due to a manufacturer's error. In my projects, I've been able to make a significant positive impact on network health and reduce the need for device replacements.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default About;
